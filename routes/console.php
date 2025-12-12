daff8e6d9d58e26bd59b3673b73cc0dc3e89e33e
Aditya Tripathi <aditya@climactic.co>
2025-12-12 19:45:36 +0000

    chore: remove inspire command

--- routes/console.php
+++ routes/console.php
@@ -6,9 +6,9 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote');
 
 // Schedule Day News ad expiration checks
 Schedule::command('ads:expire')->hourly();
