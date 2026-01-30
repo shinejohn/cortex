6bb6e21461bc5284a5ea9bec215a5d80379a372c
shinejohn <john_shine@hotmail.com>
2026-01-30 06:54:00 -0500

    Bundle all dependencies in SSR build

--- vite.config.ts
+++ vite.config.ts
@@ -23,4 +23,7 @@ export default defineConfig({
             "ziggy-js": resolve(__dirname, "vendor/tightenco/ziggy/dist/index.js"),
         },
     },
+    ssr: {
+        noExternal: true,
+    },
 });
